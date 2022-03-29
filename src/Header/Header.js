2cbaf8be5fd8a3dbd7af06423b54eebcf877643e
John Hayes <warwickcreativestudios@gmail.com>
2022-03-29 12:53:08 -0400

    adding custom css to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,44 +1,42 @@
 import React from 'react'
-import { styled } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import { Navbar } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
+import './headercss.css'
+// import { styled } from '@mui/material/styles';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
 // import MenuIcon from '@mui/icons-material/Menu';
 // import SearchIcon from '@mui/icons-material/Search';
 // import MoreIcon from '@mui/icons-material/MoreVert';
 
 const Header = () =>{
 
-    const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-        alignItems: 'flex-start',
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(2),
-        // Override media queries injected by theme.mixins.toolbar
-        '@media all': {
-          minHeight: 128,
-        },
-      }));
-      
-
-
     return(
-        <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <StyledToolbar>
-          <Typography
-            variant="h3"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1 }}
-          >
-            Warwick Creative Studios
-          </Typography>
-        </StyledToolbar>
-        {/* <IconButton /> */}
-      </AppBar>
-    </Box>
+        <Navbar className="navbar" bg="navbar" expand="lg">
+        <Container>
+          
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-end">
+              {/* <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Brand href="#home"><span className='navtext'>Warwick Creative Studio</span></Navbar.Brand>
+        </Container>
+      </Navbar>
     )
 }
 
